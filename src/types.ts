90008ebbbaffe82e1e4731c4d344faf7fb24eaac
haikel fazzani <haikel.fazzani@hotmail.com>
2025-08-30 23:23:47 +0100

    2.0.3

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export type HmacAlgorithm = 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512';
 
 export type HOTPOptions = {
   algorithm?: HmacAlgorithm;
-  digits?: number;      // default: 6
+  digits?: number;
   counter?: number;
 };
 
